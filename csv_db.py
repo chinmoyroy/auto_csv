2f9a6cf7b793c1fbbd214be3377343058dd9b1af
chinmoyroy <roy.chinmoyroy24@gmail.com>
2017-02-07 01:54:16 +0530

    master

--- csv_db.py
+++ csv_db.py
@@ -16,6 +16,7 @@ only_files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 table_names=[];
 path_of_file=''
 head=''
+s=''
 for i in only_files:
          i=i[:-4]
          path_of_file=mypath+'/'+i+'.csv'
@@ -30,22 +31,28 @@ for i in only_files:
              head=row
             # print(head)
              break
-    #    """#query ='INSERT INTO '+table_name+'('
-    #    """for i in range(0, len(head)):
-    #         query = query + head[i]
+         query ='INSERT INTO '+table_name+' ('
+         for i in range(0, len(head)):
+              query = query + head[i]
+              s=s+'"%s"'
+              if i < len(head)-1:
+                  s=s+', '
 
-    #         if i < len(head) - 1:
-    #            query = query + ','
-    #    query = query + ") VALUES(%s,%s,%s,%s,%s)"
-    #    for row in csv_data:
+              if i < len(head) - 1:
+                 query = query + ' ,'
+         query=query+' ) VALUES('
+         query=query+s+')'
 
-    #        #cursor =db1.cursor()
-    #        print(query)
+         #sprint (query)
+         for row in csv_data:
+
+             cursor =db1.cursor()
+#             print(query)
     #        row=','.join(row)
     #        query=query+row
-            #cursor.execute(query,row)
-            #db1.commit()
+             cursor.execute(query,row)
+             db1.commit()
     #        print (query)
     ##        break
 
-#cursor.close()
+cursor.close()
