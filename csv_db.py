1ab017af4173e61c2ec34e88971809b619dec5fe
chinmoyroy <roy.chinmoyroy24@gmail.com>
2017-02-09 12:20:57 +0530

    Update csv_db.py

--- csv_db.py
+++ csv_db.py
@@ -18,41 +18,40 @@ path_of_file=''
 head=''
 s=''
 for i in only_files:
-         i=i[:-4]
-         path_of_file=mypath+'/'+i+'.csv'
-         csv_data = csv.reader(file(path_of_file))
-         #table_name = i
-         s=''
-         for row in csv_data:
-             createsqltable = 'CREATE TABLE IF NOT EXISTS ' + i +'('+' VARCHAR(100),'.join(row) + ' VARCHAR(100))'
-            # print (createsqltable)
-             cursor.execute(createsqltable)
-             db1.commit()
-             head=row
-            # print(head)
-             break
-         query ='INSERT INTO '+i+' ('
-         for j in range(0, len(head)):
-              query = query + head[j]
-              s=s+"%s"
-              if j < len(head)-1:
-                  s=s+', '
+            i=i[:-4]
+            path_of_file=mypath+'/'+i+'.csv'
+            csv_data = csv.reader(file(path_of_file))
+            table_name = i
+            s=''
+            for row in csv_data:
+                createsqltable = 'CREATE TABLE IF NOT EXISTS `' + table_name +'`('+' VARCHAR(100),'.join(row) + ' VARCHAR(100))'
+               # print (createsqltable)
+                cursor.execute(createsqltable)
+                db1.commit()
+                head=row
+               # print(head)
+                break
+            query ='INSERT INTO `'+table_name+'` ('
+            for j in range(0, len(head)):
+                 query = query + head[j]
+                 s=s+"%s"
+                 if j < len(head)-1:
+                     s=s+', '
 
-              if j < len(head) - 1:
-                 query = query + ' ,'
-         query=query+' ) VALUES('
-         query=query+s+')'
+                 if j < len(head) - 1:
+                    query = query + ' ,'
+            query=query+' ) VALUES('
+            query=query+s+')'
 
-         #sprint (query)
-         for row in csv_data:
+            #sprint (query)
+            for row in csv_data:
 
-             cursor =db1.cursor()
-#             print(query)
-    #        row=','.join(row)
-    #        query=query+row
-             cursor.execute(query,row)
-             db1.commit()
-    #        print (query)
-    ##        break
-
-cursor.close()
+                cursor =db1.cursor()
+        #             print(query)
+        #        row=','.join(row)
+        #        query=query+row
+                cursor.execute(query,row)
+                db1.commit()
+            #    cursor.close()
+        #        print (query)
+        ##        break
